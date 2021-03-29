a027ee755739dbc80898b87bad292870a089461c
MuhAkramAbd <33612796+MuhAkramA@users.noreply.github.com>
2021-03-29 23:19:51 +0700

    add factorial

--- cypress/integration/examples/factorial-negative(data string).js
+++ cypress/integration/examples/factorial-negative(data string).js
@@ -5,7 +5,7 @@
 describe('Quality Assurance on Simple Factorial Web App', function() {
   it('Factorial', function() {
     cy.visit("http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/")
-    cy.get('#number').type('a')//// string data
+    cy.get('#number').type('bca')//// string data
     cy.get('#getFactorial').click()
     cy.get('#resultDiv').should('be.visible').contains('Please enter an integer')
 
