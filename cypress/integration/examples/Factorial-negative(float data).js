5af1442ab56337bf252f59a6472ebc04a7f5d09d
MuhAkramAbd <33612796+MuhAkramA@users.noreply.github.com>
2021-03-29 22:05:22 +0700

    add factorial

--- cypress/integration/examples/Factorial-negative(float data).js
+++ cypress/integration/examples/Factorial-negative(float data).js
@@ -1,12 +1,13 @@
 // type definitions for Cypress object "cy"
-/// <reference types="Cypress" />
+///<reference types="Cypress" />
 
 // type definitions for custom commands like "createDefaultTodos"
 describe('Quality Assurance on Simple Factorial Web App', function() {
   it('Factorial', function() {
-    cy.visit("http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/");
-    cy.get('#number').type('a') ///string type data
+    cy.visit("http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/")
+    cy.get('#number').type('12,5')///float
     cy.get('#getFactorial').click()
     cy.get('#resultDiv').should('be.visible').contains('Please enter an integer')
+
   })
-  })
+})
