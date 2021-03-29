cf5a65c42123686d63438385d03c3ea1660f65bd
MuhAkramAbd <33612796+MuhAkramA@users.noreply.github.com>
2021-03-29 20:26:45 +0700

    add factorial

---
+++ cypress/integration/examples/Factorial-input.js
@@ -0,0 +1,13 @@
+// type definitions for Cypress object "cy"
+///<reference types="Cypress" />
+
+// type definitions for custom commands like "createDefaultTodos"
+describe('Quality Assurance on Simple Factorial Web App', function() {
+  it('Factorial', function() {
+    cy.visit("http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/")
+    cy.get('#number').type('5')
+    cy.get('#getFactorial').click()
+    cy.get('#resultDiv').should('have.value','The factorial of 5 is: 120')
+
+  })
+})
