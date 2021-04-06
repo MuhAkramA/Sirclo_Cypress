59014d36470d3601a9c4f70aea3faf6e73c3f49c
MuhAkramA <robinvanakram@gmail.com>
2021-04-06 15:29:58 +0700

    commit

--- cypress/integration/examples/login testing.js
+++ cypress/integration/examples/login testing.js
@@ -2,9 +2,11 @@
 ///<reference types="Cypress" />
 
 // type definitions for custom commands like "createDefaultTodos"
-describe('Quality Assurance on Simple Factorial Web App', function() {
+describe('Quality Assurance on Sirclo', function() {
   it('Factorial', function() {
     cy.visit("http://qa-interview.srcli.xyz/login")
-
+    cy.get('[type="text"]').type("root")
+    cy.get('[type="password"]').type("root123")
+    cy.get('[type="submit"]').type()
   })
 })
